92fcd102258b1d6a9dff7a5fda4e964a12bc29a0
bryce <bryce.hoehn@icloud.com>
2026-04-23 22:27:40 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Radarr distroless image";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = builtins.currentSystem;
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {
+      radarr-image = pkgs.dockerTools.buildLayeredImage {
+        name = "radarr";
+        tag = "latest";
+        contents = [ 
+          pkgs.radarr
+          pkgs.cacert
+          pkgs.tzdata
+        ];
+        config = {
+          Env = [
+            "COMPlus_EnableDiagnostics=0"
+            "TMPDIR=/run/radarr-temp"
+          ];
+          ExposedPorts = {
+            "7878/tcp" = {};
+          };
+          Volumes = {
+            "/config" = {};
+            "/data" = {};
+          };
+          # Tell radarr to use /config as its data directory
+          Cmd = [ "${pkgs.radarr}/bin/radarr" "-data=/config" "-nobrowser" ];
+          # Distroless non‑root user
+          User = "1000";
+          WorkingDir = "/config";
+        };
+      };
+    };
+
+    # Expose the radarr version for CI workflows
+    radarrVersion = pkgs.radarr.version;
+
+    defaultPackage.${system} = self.packages.${system}.radarr-image;
+  };
+}
