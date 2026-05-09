de72555a64fa0a1df6e11d9a6129515856c51a0c
bryce <bryce.hoehn@icloud.com>
2026-05-08 21:16:39 -0400

    cleanup: remove User and WorkingDir, rename compose file, fix service name

--- flake.nix
+++ flake.nix
@@ -32,9 +32,7 @@
           };
           # Tell radarr to use /config as its data directory
           Cmd = [ "${pkgs.radarr}/bin/Radarr" "-data=/config" "-nobrowser" ];
-          # Distroless non‑root user
-          User = "1000";
-          WorkingDir = "/config";
+
         };
       };
     };
